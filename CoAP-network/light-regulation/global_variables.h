04035d1254d844739599bd4995e3ac14ea156d68
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-07-03 11:15:47 +0200

    added light regulation

---
+++ CoAP-network/light-regulation/global_variables.h
@@ -0,0 +1,2 @@
+extern int light_level;
+extern bool manual;
---
+++ CoAP-network/light-regulation/resources/res-crepuscular_sensor.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <math.h>
+#include "contiki.h"
+#include "coap-engine.h"
+#include "dev/leds.h"
+#include "sys/node-id.h"
+ 
+#include "global_variables.h"
+ 
+#include "sys/log.h"
+ 
+/* Log configuration */
+#define LOG_MODULE "crepuscular-sensor"
+#define LOG_LEVEL LOG_LEVEL_APP
+ 
+static void lux_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void lux_event_handler(void);
+ 
+EVENT_RESOURCE(res_crepuscular_sensor,
+         "title=\"LUX sensor\";obs",
+         lux_get_handler,
+         NULL,
+         NULL,
+         NULL,
+	     lux_event_handler);
+ 
+static int lux = 1600;
+static char sensorType[20] = "crepuscularSensor";
+ 
+static bool simulate_lux_values () { 
+	bool updated = false;
+	int old_lux = lux_level;
+ 
+    srand(time(NULL));
+    int value = 0;
+ 
+	if(light_on) {
+        lux = rand()%25000
+    }
+ 
+	if(old_ph != ph_level)
+		updated = true;
+ 
+	return updated;
+}
+ 
+static void ph_event_handler(void) {
+	if (simulate_ph_values()) { // if the value is changed
+		LOG_INFO("LUX: %d \n", lux);
+		// Notify all the observers
+    	coap_notify_observers(&res_crepuscular_sensor);
+	}
+}
+ 
+ 
+static void ph_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+  	  	char message[96];
+      	int length = 96;
+      	snprintf(message, length, "{\"node\": %d, \"value\": %d, \"manual\": %d, \"sensorType\": \"%s\"}", (unsigned int) node_id, (unsigned int)lux, (int)manual, sensorType);
+ 
+      	size_t len = strlen(message);
+      	memcpy(buffer, (const void *) message, len);
+ 
+        LOG_INFO("message: %s\n", message);
+ 
+      	coap_set_header_content_format(response, TEXT_PLAIN);
+      	coap_set_header_etag(response, (uint8_t *)&len, 1);
+      	coap_set_payload(response, buffer, len);
+}
