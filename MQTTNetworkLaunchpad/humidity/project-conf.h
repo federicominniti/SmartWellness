74318194f3690cde55a8fdf690b96b5c470e06bb
matteodelseppia <71389092+matteodelseppia@users.noreply.github.com>
2022-07-10 11:26:44 +0200

    added launchpad humidifier

--- MQTTNetworkLaunchpad/humidity/project-conf.h
+++ MQTTNetworkLaunchpad/humidity/project-conf.h
@@ -8,4 +8,11 @@
 
 #define LOG_LEVEL_APP LOG_LEVEL_DBG
 
+#define CCXXWARE_CONF_ROM_BOOTLOADER_ENABLE 1
+
+#define LOG_LEVEL_APP LOG_LEVEL_DBG
+/* Change to match your configuration */
+#define IEEE802154_CONF_PANID            0xABCD
+#define IEEE802154_CONF_DEFAULT_CHANNEL      25
+#define RF_BLE_CONF_ENABLED                   1
 #endif /* PROJECT_CONF_H_ */
