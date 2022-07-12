23ad0d4833f77a6a616ce6bbe0e055c314609d36
matteodelseppia <71389092+matteodelseppia@users.noreply.github.com>
2022-07-12 09:59:57 +0200

    updated project confs

--- CoAP-network/air-conditioning/project-conf.h
+++ CoAP-network/air-conditioning/project-conf.h
@@ -31,14 +31,14 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-// Set the max response payload before enable fragmentation:
+
 #undef REST_MAX_CHUNK_SIZE
 #define REST_MAX_CHUNK_SIZE 96
-// Set the maximum number of CoAP concurrent transactions:
+
 #undef COAP_MAX_OPEN_TRANSACTIONS
 #define COAP_MAX_OPEN_TRANSACTIONS 4
 
-/* Save some memory for the sky platform. */
+
 #undef NBR_TABLE_CONF_MAX_NEIGHBORS
 #define NBR_TABLE_CONF_MAX_NEIGHBORS 10
 
