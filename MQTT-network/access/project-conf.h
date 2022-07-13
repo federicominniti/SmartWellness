ed252a40f52d7f56e1a78e0faae7b3bdb640d896
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-07-13 10:35:18 +0200

    Update project-conf.h

--- MQTT-network/access/project-conf.h
+++ MQTT-network/access/project-conf.h
@@ -5,10 +5,4 @@
 
 #define LOG_LEVEL_APP LOG_LEVEL_DBG
 
-#ifdef  MQTT_CLIENT_CONF_LOG_LEVEL
-#define LOG_LEVEL MQTT_CLIENT_CONF_LOG_LEVEL
-#else
-#define LOG_LEVEL LOG_LEVEL_DBG
-#endif
-
 #endif /* PROJECT_CONF_H_ */
